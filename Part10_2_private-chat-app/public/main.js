1beb780941d8af6d735080474a0021419fa894c5
HoyeonKim <cabinkhy@gm.gist.ac.kr>
2023-08-08 13:22:34 +0900

    private chatting doing...

---
+++ Part10_2_private-chat-app/public/main.js
@@ -0,0 +1,164 @@
+const socket = io('http://localhost:4000', {
+    autoConnect: false
+});
+
+socket.onAny((event, ...args) => {
+    console.log(event, ...args);
+})
+
+// 전역 변수들
+const chatBody = document.querySelector('.chat-body');
+const userTitle = document.querySelector('#user-title');
+const loginContainer = document.querySelector('.login-container');
+const userTable = document.querySelector('.users');
+const userTagline = document.querySelector('#users-tagline');
+const title = document.querySelector('#active-user');
+const messages = document.querySelector('.messages');
+const msgDiv = document.querySelector('.msg-form');
+
+// login form handler
+const loginForm = document.querySelector('.user-login');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username');
+    createSession(username.value.toLowerCase())
+});
+
+const createSession = async (username) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username
+        })
+    }
+    await fetch('/session', options)
+        .then(res => res.json())
+        .then(data => {
+            socketConnect(data.username, data.userID);
+
+            //In localStorage set session
+            localStorage.setItem('session-username', data.username);
+            localStorage.setItem('session-userID', data.userID);
+
+            loginContainer.classList.add('d-none');
+            chatBody.classList.remove('d-none');
+            userTitle.innerHTML = data.username
+        })
+        .catch(err => console.log(err));
+};
+
+const socketConnect = async (username, userID) => {
+    socket.auth ={username, userID};
+
+    await socket.connect();
+};
+
+const setActiveUser = (element, username, userID) => {
+    title.innerHTML = username;
+    title.setAttribute('userID', userID);
+    
+    const lists = document.getElementsByClassName('socket-users');
+    for(let i = 0 ; i < lists.length ; i++) {
+        lists[i].classList.remove('table-active');
+    }
+    element.classList.add('table-active');
+
+    // select user and active message area
+    msgDiv.classList.remove('d-none');
+    messages.classList.remove('d-none');
+    messages.innerHTML = '';
+
+    socket.emit('fetch-messages', {receiver: userID});
+    const notify = document.getElementById(userID);
+    notify.classList.add('d-none');
+}
+
+const appendMessage = ({ message, time, background, position }) => {
+    let div = document.createElement('div');
+    div.classList.add('message', 'bg-opacity-25', 'm-2', 'px-2', 'py-1', background, position);
+    div.innerHTML = `<span class="msg-text">${message}</span> <span class="msg-time">${time}</span>`;
+    messages.append(div);
+    messages.scrollTo(0, messages.scrollHeight);
+}
+
+socket.on('users-data', ({ users }) => {
+    const index = users.findIndex(user => user.userID === socket.id);
+    if (index > -1) {
+        users.splice(index, 1);
+    }
+
+    // uesr table list generate
+    let ul = `<table class="table table-hover">`;
+    for(const user of users) {
+        ul += `<tr class="socket-users" onclick="setActiveUser(this, '${user.username}', '${user.userID}')"><td>${user.username}<span class="text-danger ps-1 d-none" id="${user.userID}">!</span></td></tr>`;
+    }
+    ul += `</table>`;
+
+    if(users.length > 0) {
+        userTable.innerHTML = ul;
+        userTagline.innerHTML = '접속 중인 유저';
+        userTagline.classList.remove('text-danger');
+        userTagline.classList.add('text-success');
+    } else {
+        userTagline.innerHTML = '접속 중인 유저 없음';
+        userTagline.classList.remove('text-success');
+        userTagline.classList.add('text-danger');
+    }
+})
+
+const sessUsername = localStorage.getItem('session-username');
+const sessUserId = localStorage.getItem('session-userID');
+
+if(sessUsername && sessUserId) {
+    socketConnect(sessUsername, sessUserId);
+    loginContainer.classList.add('d-none');
+    chatBody.classList.remove('d-none');
+    userTitle.innerHTML = sessUsername;
+}
+
+const msgForm = document.querySelector('.msgForm');
+const message = document.getElementById('message');
+
+msgForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const to = title.getAttribute('userID');
+    const time = new Date().toLocaleString('en-US', {
+        timeZone: 'Asia/Seoul',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    });
+
+    // message payload
+    const payload = {
+        from: socket.id,
+        to,
+        message: message.value,
+        time
+    }
+
+    socket.emit('message-to-server', payload);
+    
+    appendMessage({...payload, background: 'bg-success', position: 'right'});
+    
+    message.value = '';
+    message.focus();
+});
+
+socket.on('message-to-client', ({ from, message, time }) => {
+    const receiver = title.getAttribute('userID');
+    const notify = document.getElementById(from);
+
+    if(receiver === null) {
+        notify.classList.remove('d-none');
+    }
+    else if(receiver === from) {
+        appendMessage({ message, time, background: 'bg-secondary', position: 'left'});
+    }
+    else {
+        notify.classList.remove('d-none');
+    }
+});
---
+++ Part10_2_private-chat-app/src/index.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const crypto = require('crypto');
+
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const { Server } = require('socket.io');
+const server = http.createServer(app);
+const io = new Server(server);
+
+const { saveMessages } = require('./utils/messages');
+
+const publicDir = path.join(__dirname, '../public');
+app.use(express.static(publicDir));
+app.use(express.json());
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://hoyeon:1234@express-cluster.rez6pg8.mongodb.net/')
+    .then(() => console.log('connected to database'))
+    .catch((err) => console.log(err));
+
+const randomId = () => crypto.randomBytes(8).toString('hex');
+
+app.post('/session', (req, res) => {
+    const data = {
+        username: req.body.username,
+        userID: randomId(),
+    }
+    res.send(data);
+});
+
+
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username;
+    const userID = socket.handshake.auth.userID;
+
+    if(!username) {
+        return next(new Error('Invalid username'));
+    }
+
+    socket.username = username;
+    socket.id = userID;
+    next();
+});
+
+let users = [];
+io.on('connection', async socket => {
+    let userData = {
+        username: socket.username,
+        userID: socket.id
+    };
+    users.push(userData);
+    io.emit('users-data', {users});
+    
+    // 클라이언트에서 보내온 메시지
+    socket.on('message-to-server', (payload) => {
+        io.to(payload.to).emit('message-to-client', payload);
+        saveMessages(payload);
+    });
+
+    // 데이터베이스에서 메시지 가져오기
+    socket.on('fetch-messages', (data) => {
+        console.log(data);
+    });
+
+    // 유저가 방에서 나갔을 때
+    socket.on('disconnect', (data) => {
+        console.log(data);
+    });
+});
+
+
+
+const port = 4000;
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
